f622fbd8a51bc01d9a0a9264d8c08f54624114ae
ivan1309 <66654826+ivan1309@users.noreply.github.com>
2020-06-09 19:46:20 -0500

    modif1
    modificacion de index, implementación de inicio de sesión, creacion de cuenta y conexión a base de datos

---
+++ php/login.php
@@ -0,0 +1,22 @@
+<?php 
+
+	session_start();
+	require_once "conexion.php";
+
+	$conexion=conexion();
+
+		$correo=$_POST['correo'];
+		$pass=($_POST['password']);
+
+		$sql="SELECT * from usuario where correo='$correo' and password='$pass'";
+		$result=mysqli_query($conexion,$sql);
+
+		if(mysqli_num_rows($result) > 0){
+			$_SESSION['user']=$correo;
+			$sql1="SELECT nombre from usuario where correo='$correo' and password='$pass'";
+			echo "$sql1";
+			echo 1;
+		}else{
+			echo 0;
+		}
+ ?>
---
+++ php/registro.php
@@ -0,0 +1,34 @@
+<?php 
+
+	require_once "conexion.php";
+	$conexion=conexion();
+
+		
+		$nombre=$_POST['nombre'];
+		$correo=$_POST['correo'];
+		$pass=sha1($_POST['password']);
+		$foto=$_POST['foto']
+		$cc=$_POST['cc']
+
+		if(buscaRepetido($correo,$password,$conexion)==1){
+			echo 2;
+		}else{
+			$sql="INSERT into usuario ( ,correo,password,nombre,foto,cc)
+				values (,'$correo','$pass','$nombre','$foto','$cc')";
+			echo $result=mysqli_query($conexion,$sql);
+		}
+
+
+		function buscaRepetido($user,$pass,$conexion){
+			$sql="SELECT * from usuario 
+				where correo='$correo' and password='$pass'";
+			$result=mysqli_query($conexion,$sql);
+
+			if(mysqli_num_rows($result) > 0){
+				return 1;
+			}else{
+				return 0;
+			}
+		}
+
+ ?>
