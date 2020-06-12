42a33dc07439a4b1106548b35e0ef666368c653b
joshuaRiv <brayanrivera10@gmail.com>
2020-06-11 22:26:37 -0500

    CSS
    Página de búsqueda
    1/2 Implementación ajax principal

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,42 @@
-// Javascript
+// ajax Menú principal
+$(document).ready(function() {
+  var contenedor = $('#content');
+
+  /*PARA QUE CUANDO EL USUARIO QUIERA VER UN TORNEO*/
+  var buscar = $('#buscar');
+
+  buscar.click(function() {
+    //alert("tabla torneos");
+    var espera = 200;
+    $.ajax({
+      url: "../Buscar/buscar.html",
+      beforeSend: function() {
+        contenedor.text('Cargando...');
+      },
+      success: function(datos) {
+        setTimeout(function() {
+          contenedor.html(datos);
+        }, espera);
+      }
+    });
+  });
+
+
+  var home = $('#home');
+
+  home.click(function() {
+    //alert("tabla torneos");
+    var espera = 200;
+    $.ajax({
+      url: "../admiTorneos/tablaTorneos.html",
+      beforeSend: function() {
+        contenedor.text('Cargando...');
+      },
+      success: function(datos) {
+        setTimeout(function() {
+          contenedor.html(datos);
+        }, espera);
+      }
+    });
+  });
+});
